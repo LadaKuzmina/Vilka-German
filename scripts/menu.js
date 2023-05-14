d7ce91f9ecd09671ccc712b39bb96c56aace947a
senyapekus <ksenya.pshenichnikova03@gmail.com>
2023-05-14 20:28:42 +0500

    correct header in catalog

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,8 +1,10 @@
 let currentUrl = window.location.href;
 let menuLinks = document.querySelectorAll('.nav_link');
 
+//всякие каталоги лучше в будущем в отдельный список
+
 for (let i = 0; i < menuLinks.length; i++) {
-    if (menuLinks[i].href === currentUrl) {
+    if (menuLinks[i].href === currentUrl || currentUrl.includes('catalog')) {
         menuLinks[i].classList.add('active');
         break;
     }

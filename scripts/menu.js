689db4991523679ceaff553886040b46ec91fc36
senyapekus <ksenya.pshenichnikova03@gmail.com>
2023-06-21 00:27:57 +0500

    fix catalog

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,5 +1,13 @@
 let currentUrl = window.location.href;
 let menuLinks = document.querySelectorAll('.nav_link');
+let catalogLinks = document.querySelectorAll('.catalog_link');
+
+for (let i = 0; i < catalogLinks.length; i++) {
+    if (catalogLinks[i].href === currentUrl) {
+        menuLinks[0].classList.add('active');
+        break;
+    }
+}
 
 //всякие каталоги лучше в будущем в отдельный список
 

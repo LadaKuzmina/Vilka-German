b9d5a17b934efcddaa317cb3ff10b85b55dc284a
senyapekus <ksenya.pshenichnikova03@gmail.com>
2023-06-25 07:49:11 +0500

    adaptivka in header_page + dropdown

--- scripts/header.js
+++ scripts/header.js
@@ -7,65 +7,65 @@ logoDiv.classList.add('logo');
 const dropNav = document.createElement('nav');
 dropNav.classList.add('drop_nav');
 
-const dropdown = document.createElement('div');
-dropdown.classList.add('dropdown_mobile');
+const dropdownMobile = document.createElement('div');
+dropdownMobile.classList.add('dropdown_mobile');
 
 const dropButton = document.createElement('button');
 dropButton.classList.add('drop_button');
 
-const roofImg = document.createElement('img');
-roofImg.setAttribute('src', '../header_icons/roof.png');
-roofImg.setAttribute('alt', 'roof');
-roofImg.classList.add('roof');
-roofImg.setAttribute('height', '25');
-roofImg.setAttribute('width', '50');
+const mobileRoofImg = document.createElement('img');
+mobileRoofImg.setAttribute('src', '../header_icons/roof.png');
+mobileRoofImg.setAttribute('alt', 'roof');
+mobileRoofImg.classList.add('roof');
+mobileRoofImg.setAttribute('height', '25');
+mobileRoofImg.setAttribute('width', '50');
 
-dropButton.appendChild(roofImg);
+dropButton.appendChild(mobileRoofImg);
 
-const dropdownContent = document.createElement('div');
-dropdownContent.classList.add('mobile_content');
+const mobileContent = document.createElement('div');
+mobileContent.classList.add('mobile_content');
 
 const catalogLink = document.createElement('a');
 catalogLink.setAttribute('href', 'index.html');
 catalogLink.textContent = 'Каталог';
-dropdownContent.appendChild(catalogLink);
+mobileContent.appendChild(catalogLink);
 
-const aboutUsLink = document.createElement('a');
-aboutUsLink.setAttribute('href', 'page_about_us.html');
-aboutUsLink.textContent = 'О нас';
-dropdownContent.appendChild(aboutUsLink);
+const mobileAboutUsLink = document.createElement('a');
+mobileAboutUsLink.setAttribute('href', 'page_about_us.html');
+mobileAboutUsLink.textContent = 'О нас';
+mobileContent.appendChild(mobileAboutUsLink);
 
-const deliveryLink = document.createElement('a');
-deliveryLink.setAttribute('href', 'delivery_page.html');
-deliveryLink.textContent = 'Доставка';
-dropdownContent.appendChild(deliveryLink);
+const mobileDeliveryLink = document.createElement('a');
+mobileDeliveryLink.setAttribute('href', 'delivery_page.html');
+mobileDeliveryLink.textContent = 'Доставка';
+mobileContent.appendChild(mobileDeliveryLink);
 
-const contactLink = document.createElement('a');
-contactLink.setAttribute('href', 'contact_page.html');
-contactLink.textContent = 'Контакты';
-dropdownContent.appendChild(contactLink);
+const mobileContactLink = document.createElement('a');
+mobileContactLink.setAttribute('href', 'contact_page.html');
+mobileContactLink.textContent = 'Контакты';
+mobileContent.appendChild(mobileContactLink);
 
-const d3Div = document.createElement('div');
-d3Div.classList.add('d3');
+const mobileD3Div = document.createElement('div');
+mobileD3Div.classList.add('d3');
 
-const searchForm = document.createElement('form');
-searchForm.classList.add('search');
+const mobileSearchForm = document.createElement('form');
+mobileSearchForm.classList.add('search');
 
-const searchInput = document.createElement('input');
-searchInput.setAttribute('type', 'text');
-searchInput.setAttribute('placeholder', 'Искать здесь...');
+const mobileSearchInput = document.createElement('input');
+mobileSearchInput.setAttribute('type', 'text');
+mobileSearchInput.setAttribute('placeholder', 'Искать здесь...');
 
-const searchButton = document.createElement('button');
-searchButton.setAttribute('type', 'submit');
+const mobileSearchButton = document.createElement('button');
+mobileSearchButton.setAttribute('type', 'submit');
 
-searchForm.appendChild(searchInput);
-searchForm.appendChild(searchButton);
-d3Div.appendChild(searchForm);
-dropdownContent.appendChild(d3Div);
+mobileSearchForm.appendChild(mobileSearchInput);
+mobileSearchForm.appendChild(mobileSearchButton);
+mobileD3Div.appendChild(mobileSearchForm);
+mobileContent.appendChild(mobileD3Div);
 
-dropdown.appendChild(dropButton);
-dropdown.appendChild(dropdownContent);
-dropNav.appendChild(dropdown);
+dropdownMobile.appendChild(dropButton);
+dropdownMobile.appendChild(mobileContent);
+dropNav.appendChild(dropdownMobile);
 logoDiv.appendChild(dropNav);
 
 const logoLink = document.createElement('a');
@@ -155,57 +155,126 @@ headerInnerDiv.classList.add('header_inner');
 const nav = document.createElement('nav');
 nav.classList.add('nav');
 
-const catalogNavLink = document.createElement('a');
-catalogNavLink.classList.add('nav_link');
-catalogNavLink.setAttribute('href', 'index.html');
+const dropdownDiv = document.createElement('div');
+dropdownDiv.classList.add('dropdown');
 
-const catalogNavImg = document.createElement('img');
-catalogNavImg.setAttribute('src', '../header_icons/roof.png');
-catalogNavImg.setAttribute('alt', 'roof');
-catalogNavImg.classList.add('roof');
-catalogNavImg.setAttribute('height', '25');
-catalogNavImg.setAttribute('width', '50');
-catalogNavLink.appendChild(catalogNavImg);
-// catalogNavLink.appendChild(roofImg);
-catalogNavLink.innerHTML += 'Каталог';
+const dropdownButton = document.createElement('button');
+dropdownButton.classList.add('dropdown_button');
+dropdownButton.textContent = 'Каталог';
+dropdownButton.setAttribute('onclick', 'getDropdown()');
 
-nav.appendChild(catalogNavLink);
+const roofImg = document.createElement('img');
+roofImg.setAttribute('src', '../header_icons/roof.png');
+roofImg.setAttribute('alt', 'roof');
+roofImg.classList.add('roof');
+roofImg.setAttribute('height', '25');
+roofImg.setAttribute('width', '50');
 
-const aboutUsNavLink = document.createElement('a');
-aboutUsNavLink.classList.add('nav_link');
-aboutUsNavLink.setAttribute('href', 'page_about_us.html');
-aboutUsNavLink.textContent = 'О нас';
-nav.appendChild(aboutUsNavLink);
+dropdownButton.prepend(roofImg);
 
-const deliveryNavLink = document.createElement('a');
-deliveryNavLink.classList.add('nav_link');
-deliveryNavLink.setAttribute('href', 'delivery_page.html');
-deliveryNavLink.textContent = 'Доставка';
-nav.appendChild(deliveryNavLink);
+const dropdownContentDiv = document.createElement('div');
+dropdownContentDiv.classList.add('dropdown_content');
+dropdownContentDiv.setAttribute('id', 'dropdown');
 
-const contactNavLink = document.createElement('a');
-contactNavLink.classList.add('nav_link');
-contactNavLink.setAttribute('href', 'contact_page.html');
-contactNavLink.textContent = 'Контакты';
-nav.appendChild(contactNavLink);
+const roofLink = document.createElement('a');
+roofLink.classList.add('nav_link');
+roofLink.setAttribute('href', '../catalog/roof.html');
+roofLink.textContent = 'Кровля';
+dropdownContentDiv.appendChild(roofLink);
 
-const d3NavDiv = document.createElement('div');
-d3NavDiv.classList.add('d3');
+const facadeLink = document.createElement('a');
+facadeLink.classList.add('nav_link');
+facadeLink.setAttribute('href', '../catalog/facade.html');
+facadeLink.textContent = 'Фасад';
+dropdownContentDiv.appendChild(facadeLink);
 
-const searchNavForm = document.createElement('form');
-searchNavForm.classList.add('search');
+const waterSystemLink = document.createElement('a');
+waterSystemLink.classList.add('nav_link');
+waterSystemLink.setAttribute('href', '#');
+waterSystemLink.textContent = 'Водосточные системы';
+dropdownContentDiv.appendChild(waterSystemLink);
 
-const searchNavInput = document.createElement('input');
-searchNavInput.setAttribute('type', 'text');
-searchNavInput.setAttribute('placeholder', 'Искать здесь...');
+const dormerWindowLink = document.createElement('a');
+dormerWindowLink.classList.add('nav_link');
+dormerWindowLink.setAttribute('href', '../catalog/dormer_windows.html');
+dormerWindowLink.textContent = 'Мансардные окна';
+dropdownContentDiv.appendChild(dormerWindowLink);
 
-const searchNavButton = document.createElement('button');
-searchNavButton.setAttribute('type', 'submit');
+const ventilationLink = document.createElement('a');
+ventilationLink.classList.add('nav_link');
+ventilationLink.setAttribute('href', '../catalog/ventilation.html');
+ventilationLink.textContent = 'Вентиляция';
+dropdownContentDiv.appendChild(ventilationLink);
 
-searchNavForm.appendChild(searchNavInput);
-searchNavForm.appendChild(searchNavButton);
-d3NavDiv.appendChild(searchNavForm);
-nav.appendChild(d3NavDiv);
+const atticStairsLink = document.createElement('a');
+atticStairsLink.classList.add('nav_link');
+atticStairsLink.setAttribute('href', '#');
+atticStairsLink.textContent = 'Чердачные лестницы';
+dropdownContentDiv.appendChild(atticStairsLink);
+
+const insulationMaterialsLink = document.createElement('a');
+insulationMaterialsLink.classList.add('nav_link');
+insulationMaterialsLink.setAttribute('href', '../catalog/insulation_materials.html');
+insulationMaterialsLink.textContent = 'Изоляционные материалы';
+dropdownContentDiv.appendChild(insulationMaterialsLink);
+
+const fencesLink = document.createElement('a');
+fencesLink.classList.add('nav_link');
+fencesLink.setAttribute('href', '../catalog/fences.html');
+fencesLink.textContent = 'Ограждения';
+dropdownContentDiv.appendChild(fencesLink);
+
+const accessoriesLink = document.createElement('a');
+accessoriesLink.classList.add('nav_link');
+accessoriesLink.setAttribute('href', '../catalog/accessories.html');
+accessoriesLink.textContent = 'Комплектующие';
+dropdownContentDiv.appendChild(accessoriesLink);
+
+const landscapingSiteLink = document.createElement('a');
+landscapingSiteLink.classList.add('nav_link');
+landscapingSiteLink.setAttribute('href', '../catalog/landscaping_site.html');
+landscapingSiteLink.textContent = 'Благоустройство';
+dropdownContentDiv.appendChild(landscapingSiteLink);
+
+dropdownDiv.appendChild(dropdownButton);
+dropdownDiv.appendChild(dropdownContentDiv);
+nav.appendChild(dropdownDiv);
+
+const aboutUsLink = document.createElement('a');
+aboutUsLink.classList.add('nav_link');
+aboutUsLink.setAttribute('href', 'page_about_us.html');
+aboutUsLink.textContent = 'О нас';
+nav.appendChild(aboutUsLink);
+
+const deliveryLink = document.createElement('a');
+deliveryLink.classList.add('nav_link');
+deliveryLink.setAttribute('href', 'delivery_page.html');
+deliveryLink.textContent = 'Доставка';
+nav.appendChild(deliveryLink);
+
+const contactLink = document.createElement('a');
+contactLink.classList.add('nav_link');
+contactLink.setAttribute('href', 'contact_page.html');
+contactLink.textContent = 'Контакты';
+nav.appendChild(contactLink);
+
+const d3Div = document.createElement('div');
+d3Div.classList.add('d3');
+
+const searchForm = document.createElement('form');
+searchForm.classList.add('search');
+
+const searchInput = document.createElement('input');
+searchInput.setAttribute('type', 'text');
+searchInput.setAttribute('placeholder', 'Искать здесь...');
+
+const searchButton = document.createElement('button');
+searchButton.setAttribute('type', 'submit');
+
+searchForm.appendChild(searchInput);
+searchForm.appendChild(searchButton);
+d3Div.appendChild(searchForm);
+nav.appendChild(d3Div);
 
 headerInnerDiv.appendChild(nav);
 containerDiv.appendChild(headerInnerDiv);

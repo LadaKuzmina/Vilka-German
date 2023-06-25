fb5fd484a6feb029c4e2df16dfdf541b1f69038c
Ilya Lagunov <ilya.lagunov2003@gmail.com>
2023-06-26 03:53:59 +0500

    Working project

--- Back/BackServer/Program.cs
+++ Back/BackServer/Program.cs
@@ -1,4 +1,49 @@
+using BackServer.Contexts;
+using BackServer.Repositories;
+using BackServer.RepositoryChangers.Implementations;
+using BackServer.RepositoryChangers.Interfaces;
+using BackServer.Services;
+using BackServer.Services.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TestContext>(options =>
+{
+
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlOptions =>
+    {
+        sqlOptions.EnableRetryOnFailure();
+    });
+});
+
+builder.Services.AddTransient<IHeadersVisitor, HeadersVisitorDb>();
+builder.Services.AddTransient<IProductVisitor, ProductsVisitorDb>();
+builder.Services.AddTransient<IProjectVisitor, ProjectsVisitorDb>();
+builder.Services.AddTransient<IPropertyVisitor, PropertiesVisitorDb>();
+builder.Services.AddTransient<ISaleVisitor, SalesVisitorDb>();
+builder.Services.AddTransient<IPhotoVisitor, PhotoVisitorDb>();
+
+builder.Services.AddTransient<IHeadersChanger, HeadersChangerDb>();
+builder.Services.AddTransient<IProjectChanger, ProjectChangerDb>();
+builder.Services.AddTransient<IProductChanger, ProductChangerDb>();
+builder.Services.AddTransient<ISaleChanger, SaleChangerDb>();
+builder.Services.AddTransient<IPropertyChanger, PropertyChangerDb>();
+builder.Services.AddTransient<IPhotoChanger, PhotoChangerDb>();
+
+builder.Services.AddTransient<IHeadersService, HeadersService>();
+builder.Services.AddTransient<IProductService, ProductService>();
+builder.Services.AddTransient<IProjectService, ProjectService>();
+builder.Services.AddTransient<IPropertyService, PropertyService>();
+builder.Services.AddTransient<ISaleService, SaleService>();
+builder.Services.AddTransient<IPhotoService, PhotoService>();
+
+
 
 // Add services to the container.
 
@@ -6,6 +51,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -15,6 +61,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(bldr =>
+{
+    bldr.AllowAnyOrigin();
+    bldr.AllowAnyHeader();
+});
 
 app.UseHttpsRedirection();
 

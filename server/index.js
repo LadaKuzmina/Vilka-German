bc2882b289f87992d8a1a4590bd1a09561970820
Ilya Lagunov <ilya.lagunov2003@gmail.com>
2023-08-13 21:20:34 +0500

    Main page hover fix

--- server/index.js
+++ server/index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "header_page",'index.html'))
+    res.sendFile(__dirname + "/public/header_page/index.html");
 })
+
+app.get('/about', function (req, res) {
+    res.sendFile(__dirname + "/public/header_page/page_about_us.html");
+});
+
 app.listen(3000, ()=>{
     console.log("Server is running")
 })
